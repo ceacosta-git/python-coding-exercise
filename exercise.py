b208d5e30f30eb5904bbade611a20893e3aa41ab
Carlos Acosta <ceacosta.jobs@gmail.com>
2022-08-04 17:46:33 -0700

    feat: adds solution during interview for returning the neigbhors of a target item in a given list.

---
+++ exercise.py
@@ -0,0 +1,28 @@
+# [1,2,4,54,523,52,5,52,525,0,75,8,9,97,0,70,5,7,346,3,5]
+# 0 -> 525, 75
+#
+# first neighbors
+
+def find_first_neighbors(numbers, target):
+    neighbors = []
+
+    # target is not on numbers
+    # target is first on list
+    # target is last of list
+    # numbers is empty or None
+    # check datatypes
+    # decimals/floats
+    # correct nuumber of parameters
+    # return type
+    # test sample with multiple matches
+
+    for i, number in enumerate(numbers):
+        if number == target:
+            first_match_at = i
+            break
+
+    neighbors[0] = numbers[first_match_at-1], numbers[first_match_at + 1]
+    return neighbors
+
+find_first_neighbors([1,2,4,54,523,52,5,52,525,0,75,8,9,97,0,70,5,7,346,3,5], 0)
+
