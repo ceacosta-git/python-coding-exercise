909c57fa80b6d3922f38e5bf215f69adc542fdc0
Carlos Acosta <ceacosta.jobs@gmail.com>
2022-08-09 17:21:22 -0700

    test: adds a sample skipped failing test for a missed requirement where target is found and has only neighbor at the left

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -12,3 +12,10 @@ def test_neigbhbors_target_with_only_right_neighbor():
     test_sample = [11, 58, 99, 14]
     neighbors = find_first_neighbors(test_sample, 11)
     pytest.fail("missing requirement - what to return when target is the first element in the list?")
+
+
+@pytest.mark.skip
+def test_neigbhbors_target_with_only_left_neighbor():
+    test_sample = [11, 58, 99, 14]
+    neighbors = find_first_neighbors(test_sample, 14)
+    pytest.fail("missing requirement - what to return when target is the last element in the list?")
