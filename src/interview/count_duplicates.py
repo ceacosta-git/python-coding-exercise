71f2bb0738a8757ab294b071903ccc67cdf74923
Carlos Acosta <ceacosta.jobs@gmail.com>
2022-08-15 21:32:48 -0700

    chore: removes main from count duplicates as tests now exist

--- src/interview/count_duplicates.py
+++ src/interview/count_duplicates.py
@@ -8,14 +8,3 @@ def count_duplicates(items):
         # counts.update({item: value})
         # counts.update([(item, value)])
     return counts
-
-
-if __name__ == "__main__":
-    print(count_duplicates(['foo', 'bar', 'foo', 'bar', 'Foo', 'Bar']))
-    print(count_duplicates([1, 2, 1, 2, 3, 0]))
-    print(count_duplicates([True, False, True]))
-    print(count_duplicates(['foo', 'bar', 'foo', 'bar', 'Foo', 'Bar', 1, 2, 1, 2, 3, 0]))
-    print(count_duplicates([]))
-    print(count_duplicates(['foo', None, 1]))
-    print(count_duplicates(None))
-
