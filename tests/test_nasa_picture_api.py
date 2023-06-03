1120b9317be95004bd1a5fab2eabd49656fc82f4
Carlos Acosta <ceacosta.jobs@gmail.com>
2023-06-02 21:36:41 -0700

    feat: adds logging via pytest.ini

--- tests/test_nasa_picture_api.py
+++ tests/test_nasa_picture_api.py
@@ -1,14 +1,8 @@
 import requests
 import logging
-import logging.config
 from datetime import datetime
-from pathlib import Path
 
-print(str(Path('../logging.conf').resolve()))
-logging.config.fileConfig(str(Path('../logging.conf').resolve()))
-
-# create logger
-logger = logging.getLogger('test_nasa_picture_api')
+logging.basicConfig(level=logging.DEBUG)
 
 
 def test_get_picture_of_the_day():
@@ -16,8 +10,8 @@ def test_get_picture_of_the_day():
     api_url = 'https://api.nasa.gov/planetary/apod'
     query_params = {"date": today, "api_key": 'PTMCb8NPvJak1bsyJ5DK4cAMRegCufHXxUIzNLil'}
     todays_pic_response = requests.get(api_url, query_params)
-    logger.debug(f"response: {str(todays_pic_response)}")
+    logging.debug(f"response: {str(todays_pic_response)}")
     todays_pic_json = todays_pic_response.json()
-    logger.debug(f"response (json): {str(todays_pic_json)}")
+    logging.debug(f"response (json): {str(todays_pic_json)}")
     assert todays_pic_json["date"] == today
     assert todays_pic_json["media_type"] == "image"
