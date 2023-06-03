845c543132327f16e12a7701488e64a9b89e8f46
Carlos Acosta <ceacosta.jobs@gmail.com>
2023-06-02 20:26:17 -0700

    feat: adds logging to debug nasa get picture of the day

--- tests/test_nasa_picture_api.py
+++ tests/test_nasa_picture_api.py
@@ -1,12 +1,20 @@
 import requests
+import logging
+import logging.config
 from datetime import datetime
 
+logging.config.fileConfig('../logging.conf')
+
+# create logger
+logger = logging.getLogger('test_nasa_picture_api')
 
 def test_get_picture_of_the_day():
     today = datetime.today().strftime('%Y-%m-%d')
     api_url = 'https://api.nasa.gov/planetary/apod'
     query_params = {"date": today, "api_key": 'PTMCb8NPvJak1bsyJ5DK4cAMRegCufHXxUIzNLil'}
     todays_pic_response = requests.get(api_url, query_params)
+    logger.debug(f"response: {str(todays_pic_response)}")
     todays_pic_json = todays_pic_response.json()
+    logger.debug(f"response (json): {str(todays_pic_json)}")
     assert todays_pic_json["date"] == today
     assert todays_pic_json["media_type"] == "image"
